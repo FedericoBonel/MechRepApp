313d5349e41ba7b102670872425ac22bf3008a16
Federico Bonel Tozzi <57616024+FedericoBonel@users.noreply.github.com>
2022-11-20 14:49:25 +0900

    Corregi error gramatico

--- server/src/utils/constants/messages/RepositoryErrors.js
+++ server/src/utils/constants/messages/RepositoryErrors.js
@@ -7,7 +7,7 @@ const MESSAGES = {
         "La llave de acceso al API de ciudades fue revocada, por favor informe al administrador para su renovacion",
     PAIS_NOT_FOUND: "El pais no fue encontrado en la base de datos: ",
     UNKNOWN_API_ERROR:
-        "Un error ha sucedido en la conexion con la api, intente nuevamente y si el problema consiste por favor contacte con el administrador del sistema",
+        "Un error ha sucedido en la conexion con la api, intente nuevamente y si el problema continua por favor contacte con el administrador del sistema",
 };
 
 module.exports = MESSAGES;
